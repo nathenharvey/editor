663bfd287b0dac517b844b914f8da16df642c8aa
Seth Vargo <sethvargo@gmail.com>
2012-11-13 00:58:19 -0500

    version bump for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 CHANGELOG for Editor
 ====================
+v0.1.1
+------
+- also set $VISUAL
 
 v0.1.0
 ------
--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'sethvargo@gmail.com'
 license 'All rights reserved'
 description 'Set the system-wide default editor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
